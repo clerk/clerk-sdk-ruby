5477f269e05fbb7e9b46c2e47f565452c0c6ef15
Theodoros Orfanidis <teoulas@gmail.com>
2021-05-26 16:44:14 +0200

    Add railtie

--- lib/clerk.rb
+++ lib/clerk.rb
@@ -2,3 +2,7 @@
 
 require_relative "clerk/version"
 require_relative "clerk/sdk"
+
+if defined?(::Rails)
+  require_relative "clerk/railtie"
+end
---
+++ lib/clerk/railtie.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+#
+require_relative "rack_middleware"
+
+module Clerk
+  class Railtie < ::Rails::Railtie
+    initializer "clerk_railtie.configure_rails_initialization" do |app|
+      app.middleware.use Clerk::RackMiddleware
+    end
+  end
+end
