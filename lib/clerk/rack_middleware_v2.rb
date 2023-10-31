clerk/clerk-sdk-ruby

commit 437ce803487fa519c232ea4c66854bb8b2491300

chore: Allow use of Faraday v2
Obsoletes #34

lib/clerk/sdk.rb
@@ -40,7 +40,8 @@ module Clerk
 
     def initialize(api_key: nil, base_url: nil, logger: nil, ssl_verify: true,
                    connection: nil)
-      if connection # Inject a Faraday::Connection for testing or full control over Faraday
+      if connection
+        # Inject a Faraday::Connection for testing or full control over Faraday
         @conn = connection
         return
       else
@@ -50,7 +51,13 @@ module Clerk
                    else
                      URI(base_url)
                    end
-        api_key = api_key || Clerk.configuration.api_key
+
+        api_key ||= if Faraday::VERSION.to_i < 2
+            Clerk.configuration.api_key
+        elsif Clerk.configuration.api_key.nil?
+          -> { raise ArgumentError, "Clerk secret key is not set" }
+        end
+
         logger = logger || Clerk.configuration.logger
         @conn = Faraday.new(
           url: base_uri, headers: DEFAULT_HEADERS, ssl: {verify: ssl_verify}

clerk-sdk-ruby.gemspec
@@ -27,7 +27,7 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{\Aexe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
-  spec.add_dependency "faraday", "~> 1.4.1"
+  spec.add_dependency "faraday", ">= 1.4.1", "< 3.0"
   spec.add_dependency "jwt", '~> 2.5'
   spec.add_dependency "concurrent-ruby", "~> 1.1"
 

commit 44df40fc357f61a3c5145b6ce9ceea4c69d45457

fix: Properly set API key in Faraday 2
This fixes a regression introduced with #37, which caused the Clerk API
key to not be properly set when requesting Clerk's Backend API, when
Faraday v2 was used.

Fixes https://github.com/clerkinc/clerk-sdk-ruby/pull/37#issuecomment-1786060568

lib/clerk/rack_middleware_v2.rb
@@ -203,12 +203,15 @@ module Clerk
     end
 
     def development_or_staging?
-      Clerk.configuration.api_key.start_with?("test_") ||
-        Clerk.configuration.api_key.start_with?("sk_test_")
+      Clerk.configuration.api_key &&
+      (Clerk.configuration.api_key.start_with?("test_") ||
+        Clerk.configuration.api_key.start_with?("sk_test_"))
     end
 
     def production?
-      !development_or_staging?
+      Clerk.configuration.api_key &&
+      (Clerk.configuration.api_key.start_with?("live_") ||
+        Clerk.configuration.api_key.start_with?("sk_live_"))
     end
 
     def cross_origin_request?(req)

lib/clerk/sdk.rb
@@ -52,10 +52,10 @@ module Clerk
                      URI(base_url)
                    end
 
-        api_key ||= if Faraday::VERSION.to_i < 2
-            Clerk.configuration.api_key
-        elsif Clerk.configuration.api_key.nil?
-          -> { raise ArgumentError, "Clerk secret key is not set" }
+        api_key ||= Clerk.configuration.api_key
+
+        if Faraday::VERSION.to_i >= 2 && api_key.nil?
+          api_key = -> { raise ArgumentError, "Clerk secret key is not set" }
         end
 
         logger = logger || Clerk.configuration.logger