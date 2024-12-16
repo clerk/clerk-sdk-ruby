29681a6f1a82d5563258d80f04a495548c6eda90
Tom Milewski <me@tm.codes>
2024-12-16 17:21:03 -0500

    feat: Add playground applications

---
+++ apps/rails-full/config/application.rb
@@ -0,0 +1,31 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+# Load environment variables manually from shared .env file
+require "dotenv/rails"
+Dotenv::Rails.files.unshift(File.expand_path(File.join("..", "..", ".env")))
+
+module RailsFull
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 8.0
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+  end
+end
